b95e8a120efabb2cc8b97d754dad3ece4f1d5a33
Will Timpson <will.timpson@gmail.com>
2017-08-22 18:59:38 -0400

    almost search

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,76 +1,99 @@
-import React, { Component } from 'react';
-import GalleryPane from './elements/GalleryPane';
-import RefinePane from './elements/RefinePane';
-import PagerSet from './elements/PagerSet';
-const photos = require('../helpers').photos();
+import React, { Component } from "react";
+import GalleryPane from "./elements/GalleryPane";
+import RefinePane from "./elements/RefinePane";
+import PagerSet from "./elements/PagerSet";
+const photos = require("../helpers").photos();
 
 const PHOTOS_PER_PAGE = 6;
 class Gallery extends Component {
-	constructor(props) {
-		super(props);
-		const filteredPictures = photos.pictures;
-		const pagedPictures = paginate(1, filteredPictures);
+  constructor(props) {
+    super(props);
+    const filteredPictures = photos.pictures;
+    const pagedPictures = paginate(1, filteredPictures);
 
-		this.state = {
-			pictures: photos.pictures,
-			filteredPictures,
-			pagedPictures,
-			filters: photos.filters,
-			selected: 'None',
-			currentPage: 1
-		};
-		console.log(this.state);
-	}
+    this.state = {
+      pictures: photos.pictures,
+      filteredPictures,
+      pagedPictures,
+      filters: photos.filters,
+      selected: "None",
+      currentPage: 1
+    };
+    console.log(this.state);
+  }
 
-	handleFilterChange = e => {
-		const filteredPictures =
-			e.target.value === 'None'
-				? this.state.pictures
-				: this.state.pictures.filter(
-						picture => picture.filter === e.target.value
-					);
-		const pagedPictures = paginate(this.state.currentPage, filteredPictures);
+  handleSearchChange = e => {
+    const query = e.target.value;
+  };
 
-		// Set the new state.
-		this.setState({
-			selected: e.target.value,
-			filteredPictures,
-			pagedPictures
-		});
-	};
+  handleSortChange = e => {
+    const direction = e.target.value;
+    this.state.filteredPictures.sort((a, b) => {
+      [a, b] = [a.user.username, b.user.username];
+      if (direction == -1) [a, b] = [b, a];
+      if (a < b) return -1;
+      else if (a > b) return 1;
+      else return 0;
+    });
 
-	renderPage = page => () => {
-		const filteredPictures = this.state.filteredPictures;
-		const numPages = Math.ceil(filteredPictures.length / PHOTOS_PER_PAGE);
-		if (page <= 0 || page >= numPages + 1) return null;
-		const pagedPictures = paginate(page, filteredPictures);
-		this.setState({
-			currentPage: page,
-			pagedPictures
-		});
-	};
+    this.renderPage(this.state.currentPage)();
+  };
 
-	render() {
-		const page = this.state.currentPage;
-		return (
-			<div>
-				<RefinePane
-					{...this.state}
-					resultCount={this.state.filteredPictures.length}
-					handleFilterChange={this.handleFilterChange}
-				/>
-				<PagerSet page={page} renderPage={this.renderPage} />
-				<GalleryPane {...this.state} />
-				<PagerSet page={page} renderPage={this.renderPage} />
-			</div>
-		);
-	}
+  handleFilterChange = e => {
+    const filteredPictures = filter(e.target.value, this.state.pictures);
+    const pagedPictures = paginate(this.state.currentPage, filteredPictures);
+
+    // Set the new state.
+    this.setState({
+      selected: e.target.value,
+      filteredPictures,
+      pagedPictures
+    });
+  };
+
+  renderPage = page => () => {
+    const filteredPictures = this.state.filteredPictures;
+    const numPages = Math.ceil(filteredPictures.length / PHOTOS_PER_PAGE);
+    if (page <= 0 || page >= numPages + 1) return null;
+    const pagedPictures = paginate(page, filteredPictures);
+    this.setState({
+      currentPage: page,
+      pagedPictures
+    });
+  };
+
+  render() {
+    const page = this.state.currentPage;
+    const handlers = {
+      filter: this.handleFilterChange,
+      search: this.handleSearchChange,
+      sort: this.handleSortChange
+    };
+    return (
+      <div>
+        <RefinePane
+          {...this.state}
+          resultCount={this.state.filteredPictures.length}
+          handlers={handlers}
+        />
+        <PagerSet page={page} renderPage={this.renderPage} />
+        <GalleryPane {...this.state} />
+        <PagerSet page={page} renderPage={this.renderPage} />
+      </div>
+    );
+  }
 }
 
 function paginate(page, pictures) {
-	const start = PHOTOS_PER_PAGE * (page - 1);
-	const end = start + PHOTOS_PER_PAGE;
-	return pictures.slice(start, end);
+  const start = PHOTOS_PER_PAGE * (page - 1);
+  const end = start + PHOTOS_PER_PAGE;
+  return pictures.slice(start, end);
+}
+
+function filter(value, pictures) {
+  return value === "None"
+    ? pictures
+    : pictures.filter(picture => picture.filter === value);
 }
 
 export default Gallery;
--- src/components/elements/RefinePane.js
+++ src/components/elements/RefinePane.js
@@ -1,58 +1,49 @@
-import React from 'react';
+import React from "react";
 import {
-	Panel,
-	Form,
-	FormGroup,
-	FormControl,
-	ControlLabel
-} from 'react-bootstrap';
+  Panel,
+  Form,
+  FormGroup,
+  FormControl,
+  ControlLabel
+} from "react-bootstrap";
 
-const RefinePane = ({ filters, selected, handleFilterChange, resultCount }) =>
-	<Panel header={`Refine Results: ${resultCount}`}>
-		<Form inline>
-			<FormGroup className="col-sm-4" controlId="filterSelect">
-				<ControlLabel>Search&nbsp;&nbsp;</ControlLabel>
-				<FormControl
-					type="text"
-					placeholder={selected}
-					onChange={handleFilterChange}
-				>
-					{filters.map(filter =>
-						<option key={filter} value={filter}>
-							{filter}
-						</option>
-					)}
-				</FormControl>
-			</FormGroup>
-			<FormGroup className="col-sm-4" controlId="filterSelect">
-				<ControlLabel>Sort&nbsp;&nbsp;</ControlLabel>
-				<FormControl
-					componentClass="select"
-					placeholder={selected}
-					onChange={handleFilterChange}
-				>
-					{filters.map(filter =>
-						<option key={filter} value={filter}>
-							{filter}
-						</option>
-					)}
-				</FormControl>
-			</FormGroup>
-			<FormGroup className="col-sm-4" controlId="filterSelect">
-				<ControlLabel>Filters&nbsp;&nbsp;</ControlLabel>
-				<FormControl
-					componentClass="select"
-					placeholder={selected}
-					onChange={handleFilterChange}
-				>
-					{filters.map(filter =>
-						<option key={filter} value={filter}>
-							{filter}
-						</option>
-					)}
-				</FormControl>
-			</FormGroup>
-		</Form>
-	</Panel>;
+const RefinePane = ({ filters, selected, handlers, resultCount }) =>
+  <Panel header={`Refine Results: ${resultCount}`}>
+    <Form inline>
+      <FormGroup className="col-sm-4" controlId="filterSelect">
+        <ControlLabel>Search&nbsp;&nbsp;</ControlLabel>
+        <FormControl
+          type="text"
+          placeholder={selected}
+          onChange={handlers.search}
+        />
+      </FormGroup>
+      <FormGroup className="col-sm-4" controlId="filterSelect">
+        <ControlLabel>Sort&nbsp;&nbsp;</ControlLabel>
+        <FormControl
+          componentClass="select"
+          placeholder="Ascending"
+          onChange={handlers.sort}
+        >
+          <option value="1">Ascending</option>
+          <option value="-1">Descending</option>
+        </FormControl>
+      </FormGroup>
+      <FormGroup className="col-sm-4" controlId="filterSelect">
+        <ControlLabel>Filters&nbsp;&nbsp;</ControlLabel>
+        <FormControl
+          componentClass="select"
+          placeholder={selected}
+          onChange={handlers.filter}
+        >
+          {filters.map(filter =>
+            <option key={filter} value={filter}>
+              {filter}
+            </option>
+          )}
+        </FormControl>
+      </FormGroup>
+    </Form>
+  </Panel>;
 
 export default RefinePane;
