420b15a32a8eacdf1661938235144bd41b0fe07f
Will Timpson <will.timpson@gmail.com>
2017-08-22 11:48:08 -0400

    Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
+import Header from "./elements/Header";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to Photos</h2>
-        </div>
+      <div className="container">
+        <Header />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/components/elements/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Jumbotron } from "react-bootstrap";
+
+const Header = () =>
+  <Jumbotron>
+    <h1>Photoz</h1>
+  </Jumbotron>;
+
+export default Header;
