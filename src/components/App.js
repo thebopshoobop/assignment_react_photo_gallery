87f0ffabb58bbe0a5b71fd2520b63286d8957ba1
Andrew Senner <me@andrewsenner.com>
2017-08-22 12:15:19 -0400

    photozzzzzzzzzz

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,15 @@
-import React, { Component } from "react";
-import Header from "./elements/Header";
+import React, { Component } from 'react';
+import Header from './elements/Header';
+import Gallery from './Gallery';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <Header />
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="container">
+				<Header />
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/components/Gallery.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import photos from '../photos';
+
+class Gallery extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {}; // Redundant right now
+	}
+
+	render() {
+		return <p>foo</p>;
+	}
+}
+
+export default Gallery;
